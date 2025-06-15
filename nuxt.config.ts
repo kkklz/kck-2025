e386ce702eb21828808a3bc7b81036e042f99c20
Klaudiusz Tamioła <tamiola.klaudiusz@gmail.com>
2025-06-15 18:21:41 +0200

    Add page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,9 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'layout', mode: 'out-in' },
+    head: {
+      title: 'ExamBooster',
+    },
   },
 
   future: {
