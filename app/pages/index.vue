19854b117044be5fdd62e93af6ec37549170523d
Klaudiusz Tamioła <tamiola.klaudiusz@gmail.com>
2025-06-15 20:28:03 +0200

    Add hero section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,110 +1,157 @@
+<!-- eslint-disable vue/no-bare-strings-in-template -->
 <template>
-  <div class="mx-5 my-12">
-    <v-alert v-if="error">
-      {{ error.message }}
-    </v-alert>
+  <div>
+    <div
+      class="flex h-[110vh] relative"
+    >
+      <div class="mt-42 pa-12 lg:w-[40%]">
+        <h1 class="text-6xl font-bold mb-4">
+          Witaj w <span class="text-primary">ExamBooster</span>!
+        </h1>
 
-    <v-breadcrumbs
-      :items="breadcrumbs"
-    />
+        <h2 class="text-3xl mb-6">
+          Jedynej takiej platformie, która zamieni naukę w przygodę
+        </h2>
 
-    <HomepageCoursesSkeleton v-if="loading" />
+        <p class="text-2xl mb-8">
+          Odkryj interaktywne kursy, zdobywaj nagrody i ucz się poprzez grę.
+          Twoja droga do sukcesu zaczyna się tutaj!
+        </p>
 
-    <div class="mx-4 flex-wrap gap-8 grid xl:flex">
-      <div class="flex-[1]">
-        <v-card
-          :title="$t('courses.filter')"
-          color="secondary"
+        <v-btn
+          v-if="coursesRef"
+          size="large"
+          color="primary"
+          @click="coursesRef.scrollIntoView({'behavior': 'smooth'})"
         >
-          <v-list>
-            <v-list-item>
-              <v-text-field
-                v-model="searchByName"
-                append-inner-icon="mdi-magnify"
-                density="compact"
-                label="Wyszukaj kurs (po nazwie)"
-                hide-details
-                class="mb-4"
-              />
-
-              <v-text-field
-                v-model="searchByDescription"
-                append-inner-icon="mdi-magnify"
-                density="compact"
-                label="Wyszukaj kurs (po opisie)"
-                hide-details
-                class="my-4"
-              />
-            </v-list-item>
-
-            <v-divider />
-
-            <v-list-item>
-              <v-radio-group
-                v-model="sortByName"
-                label="Sortowanie (po nazwie)"
-                hide-details
-              >
-                <v-radio
-                  label="Rosnąco"
-                  value="asc"
-                />
-
-                <v-radio
-                  label="Malejąco"
-                  value="desc"
-                />
-              </v-radio-group>
-            </v-list-item>
-
-            <v-divider />
-
-            <v-list-item>
-              <v-checkbox
-                v-model="hideCoursesWithoutRewards"
-                label="Ukryj kursy bez nagród"
-                hide-details
-              />
-
-              <v-checkbox
-                v-model="hideEmptyCourses"
-                label="Ukryj puste kursy"
-                hide-details
-              />
-            </v-list-item>
-          </v-list>
-        </v-card>
+          {{ $t('courses.scroll-to-courses') }}
+        </v-btn>
       </div>
 
-      <div class="flex-[3]">
-        <v-card
-          class="mb-4"
-          :title="$t('courses.courses-list')"
-          color="primary"
+      <div class="h-[85%] w-[60%] relative !hidden !lg:block">
+        <div class="bg-gradient-overlay inset-0 absolute z-10" />
+
+        <v-img
+          src="/hero-image.jpg"
+          height="100%"
+          cover
+          class="relative"
         />
+      </div>
+    </div>
 
-        <div class="gap-6 grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4">
+    <div class="mx-5">
+      <v-alert v-if="error">
+        {{ error.message }}
+      </v-alert>
+
+      <v-breadcrumbs
+        :items="breadcrumbs"
+      />
+
+      <div
+        ref="coursesRef"
+        class="mx-4 flex-wrap gap-8 grid min-h-[85vh] xl:flex"
+      >
+        <div class="flex-[1]">
           <v-card
-            v-for="course in filteredCourses"
-            :key="course.id"
-            link
-            :to="`/course/${course.id}`"
-            class="pb-4 flex flex-col"
+            :title="$t('courses.filter')"
+            color="secondary"
           >
-            <v-img
-              :src="course.photoUrl || '/default-course-image.webp'"
-              height="200"
-              cover
-            />
+            <v-list>
+              <v-list-item>
+                <v-text-field
+                  v-model="searchByName"
+                  append-inner-icon="mdi-magnify"
+                  density="compact"
+                  label="Wyszukaj kurs (po nazwie)"
+                  hide-details
+                  class="my-4"
+                  variant="outlined"
+                />
 
-            <v-card-title class="text-lg text-black p-4">
-              {{ course.name }}
-            </v-card-title>
+                <v-text-field
+                  v-model="searchByDescription"
+                  append-inner-icon="mdi-magnify"
+                  density="compact"
+                  label="Wyszukaj kurs (po opisie)"
+                  hide-details
+                  class="my-4"
+                  variant="outlined"
+                />
+              </v-list-item>
 
-            <v-card-subtitle>
-              {{ course.description }}
-            </v-card-subtitle>
+              <v-divider />
+
+              <v-list-item>
+                <v-radio-group
+                  v-model="sortByName"
+                  label="Sortowanie (po nazwie)"
+                  hide-details
+                >
+                  <v-radio
+                    label="Rosnąco"
+                    value="asc"
+                  />
+
+                  <v-radio
+                    label="Malejąco"
+                    value="desc"
+                  />
+                </v-radio-group>
+              </v-list-item>
+
+              <v-divider />
+
+              <v-list-item>
+                <v-checkbox
+                  v-model="hideCoursesWithoutRewards"
+                  label="Ukryj kursy bez nagród"
+                  hide-details
+                />
+
+                <v-checkbox
+                  v-model="hideEmptyCourses"
+                  label="Ukryj puste kursy"
+                  hide-details
+                />
+              </v-list-item>
+            </v-list>
           </v-card>
+        </div>
+
+        <div class="flex-[3]">
+          <v-card
+            class="mb-4"
+            :title="$t('courses.courses-list')"
+            color="primary"
+          />
+
+          <HomepageCoursesSkeleton v-if="loading" />
+
+          <div class="gap-6 grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4">
+            <v-card
+              v-for="course in filteredCourses"
+              :key="course.id"
+              link
+              :to="`/course/${course.id}`"
+              class="pb-4 flex flex-col"
+            >
+              <v-img
+                :src="course.photoUrl || '/default-course-image.webp'"
+                height="200"
+                cover
+              />
+
+              <v-card-title class="text-lg text-black p-4">
+                {{ course.name }}
+              </v-card-title>
+
+              <v-card-subtitle>
+                {{ course.description }}
+              </v-card-subtitle>
+            </v-card>
+          </div>
         </div>
       </div>
     </div>
@@ -128,6 +175,8 @@ const searchByDescription = ref('')
 const sortByName = ref('asc')
 const hideEmptyCourses = ref(false)
 const hideCoursesWithoutRewards = ref(false)
+
+const coursesRef = ref<HTMLElement | null>(null)
 
 const filteredCourses = computed(() => {
   return courses.value
@@ -165,3 +214,13 @@ onBeforeMount(async () => {
   await courseStore.fetchCourses('all')
 })
 </script>
+
+<style scoped>
+.bg-gradient-overlay {
+  background: linear-gradient(
+    90deg,
+    rgba(var(--v-theme-background), 1) 0%,
+    rgba(var(--v-theme-primary), 0.7) 100%
+  );
+}
+</style>
