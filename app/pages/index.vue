854c7539acfae36f1f1ed1efeae3312cba52d3ac
Klaudiusz Tamioła <tamiola.klaudiusz@gmail.com>
2025-06-16 19:58:15 +0200

    Refactor landing page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,9 @@
 <!-- eslint-disable vue/no-bare-strings-in-template -->
 <template>
   <div
-    class="flex min-h-screen relative"
+    class="flex min-h-screen w-full items-center left-0 top-0 absolute"
   >
-    <div class="mt-42 pa-12 lg:w-[40%]">
+    <div class="pa-12 lg:w-[40%]">
       <h1 class="text-6xl font-bold mb-4">
         Witaj w <span class="text-primary">ExamBooster</span>!
       </h1>
@@ -49,7 +49,7 @@
       </div>
     </div>
 
-    <div class="h-[85%] w-[60%] relative !hidden !lg:block">
+    <div class="h-screen w-[60%] relative !hidden !lg:block">
       <div class="bg-gradient-overlay inset-0 absolute z-10" />
 
       <v-img
@@ -66,3 +66,14 @@
 const userStore = useUserStore()
 const { user } = storeToRefs(userStore)
 </script>
+
+<style scoped>
+.bg-gradient-overlay {
+  background: linear-gradient(
+    90deg,
+    rgba(var(--v-theme-background), 1) 0%,
+    rgba(var(--v-theme-primary), 0.3) 50%,
+    rgba(var(--v-theme-primary), 0.7) 100%
+  );
+}
+</style>
